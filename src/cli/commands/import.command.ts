471370ba2daa2ac76f7ccf2695a34c3910c0e146
Юлия Ситникова <yu.sitnikova@inbox.ru>
2024-09-29 23:19:16 +0500

    Добавляет команду `ImportCommand`

---
+++ src/cli/commands/import.command.ts
@@ -0,0 +1,29 @@
+import { TSVFileReader } from '../libs/file-reader/index.js';
+
+import { Command } from '../types/command.interface.js';
+import { CommandName } from '../types/command-name.enum.js';
+
+export class ImportCommand implements Command {
+  constructor() {
+  }
+
+  public getName(): string {
+    return CommandName.Import;
+  }
+
+  public run(...parameters: string[]): void {
+    const [filePath] = parameters;
+    const fileReader = new TSVFileReader(filePath);
+
+    try {
+      const data = fileReader.read();
+      console.log(data);
+    } catch (error: unknown) {
+      console.error(`Failed to import data from file ${filePath}`);
+
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  }
+}
